78a46d590339ebe7e26e181164992505625f4af3
Kilian Smiti <kilian@smiti.fr>
2021-05-18 02:49:02 +0200

    feat: add dynamic post

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,34 +1,91 @@
 <template>
-  <div id="home">
-    <div class="home__content">
-      <h1 class="logo home__logo">Kilian Smiti<span>.</span></h1>
-      <p class="home__description">
-        Welcome to my world, I am a <span>full-stack developer</span> living in
-        <span>France</span>, scroll to find out more about me !
+  <div id="welcome">
+    <div class="welcome__content">
+      <h1 class="logo welcome__logo">Kilian Smiti<span>.</span></h1>
+      <p class="welcome__description">
+        Welcome to my world, I am a <span>{{ descriptionText }}</span>
+        <br />
+        living in <span>France</span>, scroll to find out more about me !
       </p>
       <div class="scroll-down--wrapper">
         <div class="scroll-down"></div>
       </div>
     </div>
-    <div class="background home--background" />
+    <div class="background welcome--background" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'Welcome',
+  data() {
+    return {
+      currentDescription: 0,
+      descriptionText: 'Full-stack Developer',
+      descriptionList: [
+        'Full-stack Developer',
+        'Passionate',
+        'Self-taught',
+        'Designer',
+        'Robot',
+      ],
+    }
+  },
+  mounted() {
+    this.switchDescription()
+  },
+  methods: {
+    switchDescription() {
+      if (this.currentDescription + 1 > this.descriptionList.length - 1) {
+        this.currentDescription = 0
+      } else {
+        this.currentDescription++
+      }
+
+      this.writeDescription(false, this.descriptionText.length, 0)
+    },
+    writeDescription(write, length, i) {
+      if (!write) {
+        if (i < length) {
+          this.descriptionText = this.descriptionText.slice(0, -1)
+          i++
+          setTimeout(() => {
+            this.writeDescription(false, length, i)
+          }, 50)
+        } else {
+          this.writeDescription(
+            true,
+            this.descriptionList[this.currentDescription].length,
+            0
+          )
+        }
+      } else if (i < length) {
+        this.descriptionText =
+          this.descriptionText +
+          this.descriptionList[this.currentDescription].charAt(i)
+        i++
+        setTimeout(() => {
+          this.writeDescription(true, length, i)
+        }, 50)
+      } else {
+        setTimeout(() => {
+          this.switchDescription()
+        }, 1000)
+      }
+    },
+  },
 }
 </script>
 
 <style>
-.home--background {
+.welcome--background {
   background: url('/images/1.svg');
   background-size: cover;
-  animation: home--fade-in ease-in-out 2s 1 normal forwards;
+  animation: welcome--fade-in ease-in-out 2s 1 normal forwards;
   opacity: 0;
 }
 
-#home {
+#welcome {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -39,8 +96,8 @@ export default {
   width: 100%;
 }
 
-.home__content {
-  animation: home--bounce ease-in-out 0.25s 1 normal forwards;
+.welcome__content {
+  animation: welcome--bounce ease-in-out 0.25s 1 normal forwards;
   opacity: 0;
 }
 
@@ -51,7 +108,7 @@ export default {
   font-family: 'Open Sans', sans-serif;
 }
 
-.home__description {
+.welcome__description {
   line-height: 1.5;
   font-size: 2rem;
   max-width: 600px;
@@ -76,6 +133,7 @@ export default {
   right: 0;
   width: 30px;
 }
+
 .scroll-down::before {
   animation: scrollDownAnimation 2s infinite;
   background-color: #fff;
@@ -89,6 +147,44 @@ export default {
   top: 10px;
   width: 6px;
 }
+
+@keyframes welcome--bounce {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  80% {
+    opacity: 1;
+    transform: scale(1.025);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+@keyframes welcome--fade-in {
+  0% {
+    opacity: 0;
+    transform: scale(1.5);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+@keyframes welcome--fade-out {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(1.5);
+  }
+}
+
 @-moz-keyframes scrollDownAnimation {
   0% {
     opacity: 0;
